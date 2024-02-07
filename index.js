065f69b796b95a7c3de8ad7be42beaeed3af0e4a
zaheenrakib <rakibislambest@gmail.com>
2024-02-07 22:34:51 +0600

    Complete Problem two

---
+++ index.js
@@ -0,0 +1,40 @@
+/*--------------------------------------------------------------------------*/
+/*
+function calculateMoney(ticketSale) {
+    if (ticketSale < 0){
+       return "Invaild Number";
+    }
+    const totalEarn = ticketSale * 120;
+    const totalCost = (8 * 50) + 500;
+    const calculate = totalEarn - totalCost;
+    return calculate; 
+    
+}
+
+const ticket = calculateMoney(10);
+console.log(ticket);
+*/
+/*-----------------------------------------------------------------------------*/
+
+
+
+/*------------------------------------------------------------------------------*/
+
+/*
+
+function checkName(name) {
+    if(typeof name !== 'string'){
+        return "Invalid";
+    }
+    for ( let i = 0; i < name.length;i++){
+        if(name.slice(-1) === "A" || name.slice(-1) === "a" || name.slice(-1) === "Y" || name.slice(-1) === "y" || name.slice(-1) === "I" || name.slice(-1) === "i" || name.slice(-1) === "E" || name.slice(-1) === "e" || name.slice(-1) === "O" || name.slice(-1) === "o" || name.slice(-1) === "U" || name.slice(-1) === "u" || name.slice(-1) === "W" || name.slice(-1) === "w"){
+            return "Good Name";
+        }
+        else{
+            return "Bad Name";
+        }
+    }
+
+}
+*/
+
